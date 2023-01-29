15fcee52bb6ca90e03d4beee80fee478b3ac8022
Kevin <mamadaddyalexa@gmail.com>
2023-01-29 21:59:15 +0800

    Removed Application Section

--- index.php
+++ index.php
@@ -205,12 +205,6 @@ session_start(); //start temp session until logout/browser closed
                         </div>
                     </div>
                 </div>
-                <!-- 3 block sections ends -->
-                <div class="row">
-                    <div class="col-sm-12 text-center">
-                        <p class="pay-info">Pay by Cash on delivery</p>
-                    </div>
-                </div>
             </div>
         </section>
         <!-- How it works block ends -->
@@ -298,33 +292,7 @@ session_start(); //start temp session until logout/browser closed
             </div>
         </section>
         <!-- Featured restaurants ends -->
-        <section class="app-section">
-            <div class="app-wrap">
-                <div class="container">
-                    <div class="row text-img-block text-xs-left">
-                        <div class="container">
-                            <div class="col-xs-12 col-sm-5 right-image text-center">
-                                <figure> <img src="images/app.png" alt="Right Image" class="img-fluid"> </figure>
-                            </div>
-                            <div class="col-xs-12 col-sm-7 left-text">
-                                <h3>The Best Food Delivery App</h3>
-                                <p>Now you can make food happen pretty much wherever you are thanks to the free easy-to-use Food Delivery &amp; Takeout App.</p>
-                                <div class="social-btns">
-                                    <a href="#" class="app-btn apple-button clearfix">
-                                        <div class="pull-left"><i class="fa fa-apple"></i> </div>
-                                        <div class="pull-right"> <span class="text">Available on the</span> <span class="text-2">App Store</span> </div>
-                                    </a>
-                                    <a href="#" class="app-btn android-button clearfix">
-                                        <div class="pull-left"><i class="fa fa-android"></i> </div>
-                                        <div class="pull-right"> <span class="text">Available on the</span> <span class="text-2">Play store</span> </div>
-                                    </a>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </section>
+  
         <!-- start: FOOTER -->
         <footer class="footer-distributed">
 
