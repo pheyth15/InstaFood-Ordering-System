f6c5c0d7bafea1f42b585fe1ee6547286b1ca1d0
Kevin <mamadaddyalexa@gmail.com>
2023-01-31 07:18:14 +0800

    Locate CSS File

--- checkout.php
+++ checkout.php
@@ -46,7 +46,7 @@ else{
     <link href="css/animsition.min.css" rel="stylesheet">
     <link href="css/animate.css" rel="stylesheet">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-    <link href="css/style.css" rel="stylesheet"> </head>
+    <link href="css/styles.css" rel="stylesheet"> </head>
 <body>
     
     <div class="site-wrapper">
