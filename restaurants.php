abe0f6a3f9ab507b8da6c614272171866accb090
Kevin <mamadaddyalexa@gmail.com>
2023-01-31 07:02:02 +0800

    Update Footer

--- restaurants.php
+++ restaurants.php
@@ -168,94 +168,48 @@ session_start();
             </section>
             
             <!-- start: FOOTER -->
-            <footer class="footer">
-                <div class="container">
-                    <!-- top footer statrs -->
-                    <div class="row top-footer">
-                        <div class="col-xs-12 col-sm-3 footer-logo-block color-gray">
-                            <a href="#"> <img src="images/food-picky-logo.png" alt="Footer logo"> </a> <span>Order Delivery &amp; Take-Out </span> </div>
-                        <div class="col-xs-12 col-sm-2 about color-gray">
-                            <h5>About Us</h5>
-                            <ul>
-                                <li><a href="#">About us</a> </li>
-                                <li><a href="#">History</a> </li>
-                                <li><a href="#">Our Team</a> </li>
-                                <li><a href="#">We are hiring</a> </li>
-                            </ul>
-                        </div>
-                        <div class="col-xs-12 col-sm-2 how-it-works-links color-gray">
-                            <h5>How it Works</h5>
-                            <ul>
-                                <li><a href="#">Enter your location</a> </li>
-                                <li><a href="#">Choose restaurant</a> </li>
-                                <li><a href="#">Choose meal</a> </li>
-                                <li><a href="#">Pay via credit card</a> </li>
-                                <li><a href="#">Wait for delivery</a> </li>
-                            </ul>
-                        </div>
-                        <div class="col-xs-12 col-sm-2 pages color-gray">
-                            <h5>Pages</h5>
-                            <ul>
-                                <li><a href="#">Search results page</a> </li>
-                                <li><a href="#">User Sing Up Page</a> </li>
-                                <li><a href="#">Pricing page</a> </li>
-                                <li><a href="#">Make order</a> </li>
-                                <li><a href="#">Add to cart</a> </li>
-                            </ul>
-                        </div>
-                        <div class="col-xs-12 col-sm-3 popular-locations color-gray">
-                            <h5>Popular locations</h5>
-                            <ul>
-                                <li><a href="#">Sarajevo</a> </li>
-                                <li><a href="#">Split</a> </li>
-                                <li><a href="#">Tuzla</a> </li>
-                                <li><a href="#">Sibenik</a> </li>
-                                <li><a href="#">Zagreb</a> </li>
-                                <li><a href="#">Brcko</a> </li>
-                                <li><a href="#">Beograd</a> </li>
-                                <li><a href="#">New York</a> </li>
-                                <li><a href="#">Gradacac</a> </li>
-                                <li><a href="#">Los Angeles</a> </li>
-                            </ul>
-                        </div>
+            <footer class="footer-distributed">
+
+                <div class="footer-left">
+
+                    <div class="logo-image">
+                        <img src="assets/img/banner-footer.png">
+                    </div><br>
+
+                    <p class="footer-links">
+                        <a href="index.php">Home</a>
+                        ·
+                        <a href="#">View Stores</a>
+                        ·
+                        <a href="#">My Bag</a>
+                        ·
+                        <a href="#">Order</a>
+                    </p>
+
+                    <p class="footer-company-name">InstaFood © 2023</p>
+
+                    <div class="footer-icons">
+
+                        <a href="#"><i class="fa fa-facebook"></i></a>
+                        <a href="#"><i class="fa fa-twitter"></i></a>
+                        <a href="#"><i class="fa fa-instagram"></i></a>
+                        <a href="#"><i class="fa fa-telegram"></i></a>
                     </div>
-                    <!-- top footer ends -->
-                    <!-- bottom footer statrs -->
-                    <div class="row bottom-footer">
-                        <div class="container">
-                            <div class="row">
-                                <div class="col-xs-12 col-sm-3 payment-options color-gray">
-                                    <h5>Payment Options</h5>
-                                    <ul>
-                                        <li>
-                                            <a href="#"> <img src="images/paypal.png" alt="Paypal"> </a>
-                                        </li>
-                                        <li>
-                                            <a href="#"> <img src="images/mastercard.png" alt="Mastercard"> </a>
-                                        </li>
-                                        <li>
-                                            <a href="#"> <img src="images/maestro.png" alt="Maestro"> </a>
-                                        </li>
-                                        <li>
-                                            <a href="#"> <img src="images/stripe.png" alt="Stripe"> </a>
-                                        </li>
-                                        <li>
-                                            <a href="#"> <img src="images/bitcoin.png" alt="Bitcoin"> </a>
-                                        </li>
-                                    </ul>
-                                </div>
-                                <div class="col-xs-12 col-sm-4 address color-gray">
-                                    <h5>Address</h5>
-                                    <p>Concept design of oline food order and deliveye,planned as restaurant directory</p>
-                                    <h5>Phone: <a href="tel:+080000012222">080 000012 222</a></h5> </div>
-                                <div class="col-xs-12 col-sm-5 additional-info color-gray">
-                                    <h5>Addition informations</h5>
-                                    <p>Join the thousands of other restaurants who benefit from having their menus on TakeOff</p>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                    <!-- bottom footer ends -->
+
+                </div>
+
+                <div class="footer-right">
+
+                    <p>Contact Us</p>
+
+                    <form id="myForm" action="assets/back-end/connect-footer.php" method="post">
+
+                        <input type="text" name="email" placeholder="Email">
+                        <textarea name="message" placeholder="Message"></textarea>
+                        <button onclick="myFunction()">Send</button>
+
+                    </form>
+
                 </div>
             </footer>
             <!-- end:Footer -->
