934cf6dc9d89179ca2f4be78acdd4b56df6772a5
Kevin <mamadaddyalexa@gmail.com>
2023-02-02 11:32:15 +0800

    Add Login Background

--- login.php
+++ login.php
@@ -21,7 +21,7 @@
   
 </head>
 
-<body>
+<body class="wrapper1">
 <?php
 include("connection/connect.php"); //INCLUDE CONNECTION
 error_reporting(0); // hide undefine index errors
@@ -57,7 +57,6 @@ if(isset($_POST['submit']))   // if button is submit
 <!-- Button Mixin-->
 <!-- Pen Title-->
 <div class="pen-title">
-  <h1>Login Form</h1>
 </div>
 <!-- Form Module-->
 <div class="module form-module">
@@ -65,24 +64,21 @@ if(isset($_POST['submit']))   // if button is submit
    
   </div>
   <div class="form">
-    <h2>Login to your account</h2>
+    <h2>Log In To Your Account</h2>
 	  <span style="color:red;"><?php echo $message; ?></span> 
    <span style="color:green;"><?php echo $success; ?></span>
     <form action="" method="post">
       <input type="text" placeholder="Username"  name="username"/>
       <input type="password" placeholder="Password" name="password"/>
-      <input type="submit" id="buttn" name="submit" value="login" />
+      <input type="submit" id="buttn" name="submit" value="Log In" />
     </form>
   </div>
   
-  <div class="cta">Not registered?<a href="registration.php" style="color:#f30;"> Create an account</a></div>
+  <div class="cta">Not registered yet?<a href="registration.php" style="color:#f30;"> Create an account</a></div>
 </div>
   <script src='http://cdnjs.cloudflare.com/ajax/libs/jquery/2.1.3/jquery.min.js'></script>
 
   
-
-   
-
 
 
 </body>
