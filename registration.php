9f1bedc0e8f02efc9d34ce31ed3987478adea436
Kevin <mamadaddyalexa@gmail.com>
2023-02-02 18:17:38 +0800

    Relocate CSS File Path

--- registration.php
+++ registration.php
@@ -92,7 +92,7 @@ if(isset($_POST['submit'] )) //if submit btn is pressed
     <link href="css/animsition.min.css" rel="stylesheet">
     <link href="css/animate.css" rel="stylesheet">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-    <link href="css/style.css" rel="stylesheet"> </head>
+    <link href="css/styles.css" rel="stylesheet"> </head>
 <body>
      
          <!--header starts-->
