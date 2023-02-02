4b31159bcf47c71e848d391efcd3d14d63a5ff88
Kevin <mamadaddyalexa@gmail.com>
2023-02-03 07:32:00 +0800

    Update Footer Nav

--- stores.php
+++ stores.php
@@ -179,11 +179,11 @@ session_start();
                     <p class="footer-links">
                         <a href="index.php">Home</a>
                         ·
-                        <a href="#">View Stores</a>
+                        <a href="stores.php">View Stores</a>
                         ·
-                        <a href="#">My Bag</a>
+                        <a href="your_orders.php">My Orders</a>
                         ·
-                        <a href="#">Order</a>
+                        <a href="stores.php">Order Now</a>
                     </p>
 
                     <p class="footer-company-name">InstaFood © 2023</p>
