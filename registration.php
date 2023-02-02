e34aa7413b0aa2d593460ec154e78cf6b847e808
Kevin <mamadaddyalexa@gmail.com>
2023-02-02 20:12:39 +0800

    Update Registration

--- registration.php
+++ registration.php
@@ -209,25 +209,39 @@ if(isset($_POST['submit'] )) //if submit btn is pressed
                         <p></p>
                         <div class="panel">
                            <div class="panel-heading">
-                              <h4 class="panel-title"><a data-parent="#accordion" data-toggle="collapse" class="panel-toggle collapsed" href="#faq1" aria-expanded="false"><i class="ti-info-alt" aria-hidden="true"></i>Can I viverra sit amet quam eget lacinia?</a></h4>
+                              <h4 class="panel-title"><a data-parent="#accordion" data-toggle="collapse" class="panel-toggle collapsed" href="#faq1" aria-expanded="false"><i class="ti-info-alt" aria-hidden="true"></i>What is InstaFood?</a></h4>
                            </div>
                            <div class="panel-collapse collapse" id="faq1" aria-expanded="false" role="article" style="height: 0px;">
-                              <div class="panel-body"> Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam rutrum ut erat a ultricies. Phasellus non auctor nisi, id aliquet lectus. Vestibulum libero eros, aliquet at tempus ut, scelerisque sit amet nunc. Vivamus id porta neque, in pulvinar ipsum. Vestibulum sit amet quam sem. Pellentesque accumsan consequat venenatis. Pellentesque sit amet justo dictum, interdum odio non, dictum nisi. Fusce sit amet turpis eget nibh elementum sagittis. Nunc consequat lacinia purus, in consequat neque consequat id. </div>
+                              <div class="panel-body"> 
+                              InstaFood is one of the leading local food delivery marketplace. Users can conveniently order their favorite meals online and discover food around their local area. 
+                              <br><br>
+                              In December 2022, InstagramFood launched in the Philippines with over 10+ partners.
+                              <br><br>
+
+                              InstaFood is a team of problem solvers, designers, thinkers, and tinkers working around the clock to make it the most powerful online food delivery tool on the market today. You should be able to order food easily, quickly, and definitely with a smile on your face!
+
+
+                           </div>
                            </div>
                         </div>
                         <!-- end:panel -->
                         <div class="panel">
                            <div class="panel-heading">
-                              <h4 class="panel-title"><a data-parent="#accordion" data-toggle="collapse" class="panel-toggle" href="#faq2" aria-expanded="true"><i class="ti-info-alt" aria-hidden="true"></i>Can I viverra sit amet quam eget lacinia?</a></h4>
+                              <h4 class="panel-title"><a data-parent="#accordion" data-toggle="collapse" class="panel-toggle" href="#faq2" aria-expanded="true"><i class="ti-info-alt" aria-hidden="true"></i>How long is the validity of my account?</a></h4>
                            </div>
                            <div class="panel-collapse collapse" id="faq2" aria-expanded="true" role="article">
-                              <div class="panel-body"> Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam rutrum ut erat a ultricies. Phasellus non auctor nisi, id aliquet lectus. Vestibulum libero eros, aliquet at tempus ut, scelerisque sit amet nunc. Vivamus id porta neque, in pulvinar ipsum. Vestibulum sit amet quam sem. Pellentesque accumsan consequat venenatis. Pellentesque sit amet justo dictum, interdum odio non, dictum nisi. Fusce sit amet turpis eget nibh elementum sagittis. Nunc consequat lacinia purus, in consequat neque consequat id. </div>
+                              <div class="panel-body"> 
+                              A membership of the InstaFoodies is for a lifetime, which means that you don't have to create a new account yearly just to be a member of the InstaFoodies community. 
+                              <br><br>
+                              What are you waiting for? Join the Instafoodie community today and start enjoying the perks!
+
+                              </div>
                            </div>
                         </div>
                         <!-- end:Panel -->
                         <h4 class="m-t-20">Contact Customer Support</h4>
-                        <p> If you"re looking for more help or have a question to ask, please </p>
-                        <p> <a href="#contact" class="btn theme-btn m-t-15">contact us</a> </p>
+                        <p> Do you need assistance? Have questions or concerns? <br><br>Contact us quickly and easily by clicking the button below. </p>
+                        <p> <a href="#contact" class="btn theme-btn m-t-15">Contact Us</a> </p>
                      </div>
                      <!-- /WHY? -->
                   </div>
