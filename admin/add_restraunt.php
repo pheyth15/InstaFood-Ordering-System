b30742b052d18c1f865239555b200f266e85d640
Kevin <mamadaddyalexa@gmail.com>
2023-02-04 06:39:05 +0800

    Update Title Bar
    Co-Authored-By: Rynella <122844491+izana92@users.noreply.github.com>
    Co-Authored-By: JAPS <108985566+JapsSwain@users.noreply.github.com>

--- admin/add_restraunt.php
+++ admin/add_restraunt.php
@@ -114,8 +114,8 @@ if(isset($_POST['submit']))           //if upload btn is pressed
     <meta name="description" content="">
     <meta name="author" content="">
     <!-- Favicon icon -->
-    <link rel="icon" type="image/png" sizes="16x16" href="images/favicon.png">
-    <title>Ela - Bootstrap Admin Dashboard Template</title>
+    <link rel="icon" type="image/png" sizes="16x16" href="../assets/img/web-icon.png">
+    <title>Add Restaurant - Admin | InstaFood/title>
     <!-- Bootstrap Core CSS -->
     <link href="css/lib/bootstrap/bootstrap.min.css" rel="stylesheet">
     <!-- Custom CSS -->
