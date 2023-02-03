5c3e45f82537a406aafdeebd35640fd2ee511f85
Kevin <mamadaddyalexa@gmail.com>
2023-02-04 07:50:03 +0800

    Update NavBar2
    Co-Authored-By: Rynella <122844491+izana92@users.noreply.github.com>
    Co-Authored-By: JAPS <108985566+JapsSwain@users.noreply.github.com>

--- admin/update_users.php
+++ admin/update_users.php
@@ -173,11 +173,11 @@ if(isset($_POST['submit'] ))
                                 
                             </ul>
                         </li>
-                        <li class="nav-label">Log</li>
+                        <li class="nav-label">System Log</li>
                         <li> <a class="has-arrow  " href="#" aria-expanded="false">  <span><i class="fa fa-user f-s-20 "></i></span><span class="hide-menu">Users</span></a>
                             <ul aria-expanded="false" class="collapse">
                                 <li><a href="allusers.php">All Users</a></li>
-								<li><a href="add_users.php">Add Users</a></li>
+								<li><a href="add_users.php">Add User</a></li>
 								
                                
                             </ul>
@@ -186,13 +186,13 @@ if(isset($_POST['submit'] ))
                             <ul aria-expanded="false" class="collapse">
 								<li><a href="allrestraunt.php">All Stores</a></li>
 								<li><a href="add_category.php">Add Category</a></li>
-                                <li><a href="add_restraunt.php">Add Restaurant</a></li>
+                                <li><a href="add_restraunt.php">Add Store</a></li>
                                 
                             </ul>
                         </li>
                       <li> <a class="has-arrow  " href="#" aria-expanded="false"><i class="fa fa-cutlery" aria-hidden="true"></i><span class="hide-menu">Menu</span></a>
                             <ul aria-expanded="false" class="collapse">
-								<li><a href="all_menu.php">All Menues</a></li>
+								<li><a href="all_menu.php">All Menu</a></li>
 								<li><a href="add_menu.php">Add Menu</a></li>
                               
                                 
