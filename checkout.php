69ce57ec8b6d9a5f73651efb1ed166fde7b4e70e
Kevin <mamadaddyalexa@gmail.com>
2023-02-04 10:02:13 +0800

    Update Checkout
    Co-Authored-By: JAPS <108985566+JapsSwain@users.noreply.github.com>
    Co-Authored-By: Rynella <122844491+izana92@users.noreply.github.com>

--- checkout.php
+++ checkout.php
@@ -155,7 +155,7 @@ else{
                                             </li>
                                             <li>
                                                 <label class="custom-control custom-radio  m-b-10">
-                                                    <input name="mod"  type="radio" value="paypal" disabled class="custom-control-input"> <span class="custom-control-indicator"></span> <span class="custom-control-description">Paypal <img src="images/paypal.jpg" alt="" width="90"></span> </label>
+                                                    <input name="mod"  type="radio" value="paypal" disabled class="custom-control-input"> <span class="custom-control-indicator"></span> <span class="custom-control-description">Online Payment <img src="images/paypal.jpg" alt="" width="90"></span> </label>
                                             </li>
                                         </ul>
                                         <p class="text-xs-center"> <input type="submit" onclick="return confirm('Are you sure?');" name="submit"  class="btn btn-outline-success btn-block" value="Order now"> </p>
